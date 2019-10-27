3aaef893037269646b006d30a16ad702e25d0050
Sarah Li <crab.apples00@gmail.com>
2019-10-26 18:31:04 -0700

    latest

--- temporalDifference/td0.py
+++ temporalDifference/td0.py
@@ -15,13 +15,18 @@ N = 3;
 M = 3;
 S = N*M; A = 4;
 gamma = 0.5;
+stateVec = np.linspace(0,S,S,endpoint = False);
 P = ut.rectangleMD(M,N, 0.7)
 
 C = np.random.rand(S,A);
-
+# generate random list
 #SARSA implementation
 
 T = 3000;
-Q = np.zeros((S, A, T))
+Q = np.zeros((S, A, T));
+s = np.random.randint(0,S);
 for t in range(T):
-    
+    aNext = np.argmax(Q[s, :, t]);
+    transition = P[:,s*A + aNext];
+    nextS  = np.random.choice(stateVec, 1, p = transition);
+    nextR = 1*C(aNext, s);
